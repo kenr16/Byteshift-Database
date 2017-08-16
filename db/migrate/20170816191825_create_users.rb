d88a457ab9ab0cf8c37c918fd5ce343c5b682e63
Ken Rutan <kenr16@gmail.com>
2017-08-16 14:11:25 -0700

    changed migrations to not conflict with postgres

--- db/migrate/20170816191825_create_users.rb
+++ db/migrate/20170816191825_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :first_name
       t.string :last_name
       t.string :status
-      t.string :id
+      t.string :uuid_number
       t.string :email
 
       t.timestamps
