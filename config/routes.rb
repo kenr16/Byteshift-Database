2212d67054679619907414e38a7efd1207108154
Steven Galvin <steven.john.galvin@gmail.com>
2017-08-18 11:37:23 -0700

    remove commented out code rom routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   get 'beacon/search' => 'beacons#search'
 
   get 'visit/search' => 'visits#search'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
